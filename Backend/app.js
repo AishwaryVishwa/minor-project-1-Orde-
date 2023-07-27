333af9fde30b4833f1a94e860ca095b762206c70
AishwaryVishwa <aishwaryvishwakarma11@gmail.com>
2023-07-27 20:57:10 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,14 @@
 const express =require('express')
 const dotenv=require('dotenv')
 const cookieParser = require('cookie-parser')
+const cors = require("cors");
 // declaring path for env file only have to do in app.js root of project
 dotenv.config({path:'./config.env'})
 const app=express();
 app.use(express.json())
 
 app.use(cookieParser())
+app.use(cors());
 // making connection with the database
 require('./DATABASE/dbConnection')
 
